50dd901675470ee98c5816ecc3c060acc4a2865b
Yeonsu <102608507+Yeonsu-Sim@users.noreply.github.com>
2025-01-19 00:01:26 +0900

    [Bronze V] Title: A×B, Time: 108 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    static int A, B, result;
+    static int[] in;
+
+    public static void main(String[] args) throws IOException {
+        // 시스템으로부터 입력 받기기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] line = br.readLine().split(" ");
+
+        A = Integer.parseInt(line[0]);
+        B = Integer.parseInt(line[1]);
+
+        System.out.println(A*B);
+    }
+}
