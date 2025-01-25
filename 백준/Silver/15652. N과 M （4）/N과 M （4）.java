542226aa7a7663eb986a88dcc93cf409060019ef
Yeonsu <102608507+Yeonsu-Sim@users.noreply.github.com>
2025-01-25 10:55:46 +0900

    [Silver III] Title: N과 M (4), Time: 136 ms, Memory: 17316 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        // 시스템으로부터 입력 받기기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] line = br.readLine().split(" ");
+        N = Integer.parseInt(line[0]);
+        M = Integer.parseInt(line[1]);
+
+        arr = new int[N];
+        for (int i=0; i<N; i++) {
+            arr[i] = i+1;
+        }
+        dfs(0,1);
+        System.out.println(sb.toString());
+    }
+    static void dfs(int depth, int start) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i=start; i<N+1; i++) {
+            arr[depth] = i;
+            dfs(depth+1, i);
+        }
+    }
+}
