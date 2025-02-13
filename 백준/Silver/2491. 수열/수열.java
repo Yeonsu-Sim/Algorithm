6b03b2415e15fa18cb6f992e1494f43d0f8434d3
Yeonsu <102608507+Yeonsu-Sim@users.noreply.github.com>
2025-02-13 15:04:49 +0900

    [Silver IV] Title: 수열, Time: 236 ms, Memory: 26608 KB -BaekjoonHub

--- 백준/Silver/2491. 수열/수열.java
+++ 백준/Silver/2491. 수열/수열.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Main {
 
-    static int N, ascIdx,descIdx;
+    static int N;
     static int[] arr, asc, desc;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,34 +20,30 @@ public class Main {
             arr[i] = Integer.parseInt(line[i]);
         }
 
-        // 증가수열 초기화
+        // 수열 길이 초기화
         Arrays.fill(asc, 1);
         Arrays.fill(desc, 1);
 
-        for (int i=0; i<N-1; i++) {
-            if (arr[i] > arr[i+1]) {
-                asc[ascIdx]++;
-                descIdx++;
-            } else if (arr[i] < arr[i+1]) {
-                desc[descIdx]++;
-                ascIdx++;
-            } else {
-                asc[ascIdx]++;
-                desc[descIdx]++;
+        // 증가 수열 길이 누적
+        for (int i=1; i<N; i++) {
+            if (arr[i] >= arr[i-1]) {
+                asc[i] = asc[i-1]+1;
             }
-
         }
 
-        int ascMax = 0;
+        // 감소 수열 길이 누적
+        for (int i=1; i<N; i++) {
+            if (arr[i] <= arr[i-1]) {
+                desc[i] = desc[i-1]+1;
+            }
+        }
+
+        int max1=0, max2=0;
         for (int i=0; i<N; i++) {
-            ascMax = Math.max(ascMax, asc[i]);
+            max1 = max1 > asc[i] ? max1 : asc[i];
+            max2 = max2 > desc[i] ? max2 : desc[i];
         }
 
-        int descMax = 0;
-        for (int i=0; i<N; i++) {
-            descMax = Math.max(descMax, desc[i]);
-        }
-
-        System.out.println(Math.max(ascMax, descMax));
+        System.out.println(Math.max(max1, max2));
     }
 }
