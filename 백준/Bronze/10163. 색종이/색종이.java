7ba5c7a847a965a655483aae2f56acf30b01406e
Yeonsu <102608507+Yeonsu-Sim@users.noreply.github.com>
2025-02-13 16:47:36 +0900

    [Bronze I] Title: 색종이, Time: 196 ms, Memory: 22120 KB, Score: 42 point -BaekjoonHub

---
+++ 백준/Bronze/10163. 색종이/색종이.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, x, y, w, h, area;
+    static int[][] arr = new int[1000][1000];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+
+        // 각 색종이의 위치와 크기 입력받고 저장
+        for (int n=0; n<N; n++) {
+            String[] line = br.readLine().split(" ");
+            for (int i=0; i<4; i++) {
+                x = Integer.parseInt(line[0]);
+                y = Integer.parseInt(line[1]);
+                w = Integer.parseInt(line[2]);
+                h = Integer.parseInt(line[3]);
+            }
+
+            for (int i=0; i<h; i++) {
+                for (int j=0; j<w; j++) {
+                    arr[y+i][x+j] = n+1;
+                }
+            }
+        }
+        
+        // 각 색종이의 가시 면적 구하기
+        for (int n=0; n<N; n++) {
+            for (int i=0; i<1000; i++) {
+                for (int j=0; j<1000; j++) {
+                    if (arr[i][j] == n+1) {
+                        area++;
+                    }
+                }
+            }
+            sb.append(area).append('\n');
+            area = 0;
+        }
+
+
+        System.out.println(sb);
+    }
+}
