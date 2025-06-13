208a2f11d3b7a68a5899f8e7018c7f51daab970f
Yeonsu <102608507+Yeonsu-Sim@users.noreply.github.com>
2025-06-13 19:04:20 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 524 ms, Memory: 62388 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    StringTokenizer st;
+
+    List<Integer>[] graph = new ArrayList[N+1];
+    for (int i=1; i<=N; i++) {
+      graph[i] = new ArrayList<>();
+    }
+
+    for (int i=1; i<N; i++) {
+      st = new StringTokenizer(br.readLine());
+      int from = Integer.parseInt(st.nextToken());
+      int to = Integer.parseInt(st.nextToken());
+      graph[from].add(to);
+      graph[to].add(from);
+    }
+
+    int[] parents = new int[N+1];
+    boolean[] visited = new boolean[N+1];
+
+    Queue<Integer> q = new ArrayDeque<>();
+    q.offer(1);
+    visited[1] = true;
+
+    while (!q.isEmpty()) {
+      int cur = q.poll();
+      for (int child : graph[cur]) {
+        if (visited[child]) continue;
+        parents[child] = cur;
+        visited[child] = true;
+        q.offer(child);
+      }
+    }
+
+    StringBuilder sb = new StringBuilder();
+    for (int i=2; i<=N; i++) {
+      sb.append(parents[i]).append('\n');
+    }
+
+    System.out.println(sb);
+  }
+}
