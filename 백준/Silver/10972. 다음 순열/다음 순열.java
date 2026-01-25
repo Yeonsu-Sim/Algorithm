a753821561a1359971310d26d3e15a01871a739f
Yeonsu <102608507+Yeonsu-Sim@users.noreply.github.com>
2026-01-25 22:12:55 +0900

    [Silver III] Title: 다음 순열, Time: 320 ms, Memory: 20088 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    
+    int[] arr = new int[N];
+    for (int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+    for (int i=N-1; i>0; i--) {
+      int target = arr[i-1];
+
+      if (arr[i] > target) {
+        for (int j=i+1; j<N; j++) {
+          if (arr[j] > target && arr[i] > arr[j]) {
+            int tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+          }
+        }
+
+        int tmp = arr[i];
+        arr[i] = target;
+        arr[i-1] = tmp;
+        
+        for (int j=0; j<i; j++) {
+          System.out.print(arr[j]+" ");
+        }
+
+        for (int j=i; j<N; j++) pq.offer(arr[j]);
+
+        while (!pq.isEmpty()) System.out.print(pq.poll()+" ");
+
+        return;
+      }
+    }
+
+    System.out.println(-1);
+  }
+}
