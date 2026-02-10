ab4b517f811c51ee5d9f8d24ab9c64dd551c4ba4
Yeonsu <102608507+Yeonsu-Sim@users.noreply.github.com>
2026-02-10 14:40:20 +0900

    [Gold IV] Title: 저울, Time: 132 ms, Memory: 15000 KB -BaekjoonHub

--- 백준/Gold/10159. 저울/저울.java
+++ 백준/Gold/10159. 저울/저울.java
@@ -1,62 +1,51 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine().trim());
+		boolean[][] arr = new boolean[n + 1][n + 1];
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int m = Integer.parseInt(br.readLine().trim());
+		for (int i = 0; i < m; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-        StringTokenizer st;
+			arr[a][b] = true;
+		}
+		
+		for (int k = 1; k <= n; k++) {
+			for (int i = 1; i <= n; i++) {
+				if (!arr[i][k])
+					continue;
+				for (int j = 1; j <= n; j++) {
+					if (arr[k][j])
+						arr[i][j] = true;
+				}
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i=1;i<=n;i++) {
+			int unknown = 0;
+			for(int j=1;j<=n;j++) {
+				if(i == j )continue;
+				boolean ij = arr[i][j];
+				boolean ji = arr[j][i];
+				if((ij && !ji) || (!ij && ji)){
+					continue;
+				}
+				if (!ij && !ji) {
+					unknown++;
+		    }
+			}
+			sb.append(unknown).append("\n");
+		}
+		
+		System.out.println(sb.toString());
 
-        List<List<Integer>> in = new ArrayList<>();
-        List<List<Integer>> out = new ArrayList<>();
+	}
 
-        for (int i=0; i<N+1; i++) {
-            in.add(new ArrayList<>());
-            out.add(new ArrayList<>());
-        }
-
-        for (int i=0; i<M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-
-            in.get(from).add(to);
-            out.get(to).add(from);
-        }
-
-        StringBuilder sb = new StringBuilder();
-        Queue<int[]> q = new ArrayDeque<>();
-        for (int i=1; i<N+1; i++) {
-            int unknownCnt = N-1;
-            boolean[][] visited = new boolean[N+1][2];
-
-            q.offer(new int[]{i, 0});
-            q.offer(new int[]{i, 1});
-            visited[i][0] = true;
-            visited[i][1] = true;
-
-            while (!q.isEmpty()) {
-                int[] cur = q.poll();
-                int num = cur[0];
-                int dir = cur[1];
-
-                List<Integer> graph = dir > 0 ? in.get(num) : out.get(num);
-
-                for (int next : graph) {
-                    if (!visited[next][dir]) {
-                        q.offer(new int[]{next, dir});
-                        unknownCnt--;
-                        visited[next][dir] = true;
-                    }
-                }
-            }
-
-            sb.append(unknownCnt).append('\n');
-        }
-
-        System.out.println(sb);
-    }
 }
