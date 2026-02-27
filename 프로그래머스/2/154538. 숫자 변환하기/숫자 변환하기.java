65b41f55670e4aec2aaa03d150bfab2180478ed8
Yeonsu <102608507+Yeonsu-Sim@users.noreply.github.com>
2026-02-27 22:45:14 +0900

    [level 2] Title: 숫자 변환하기, Time: 27.29 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -6,10 +6,12 @@ class Solution {
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[x] = 0;
         
-        for (int i=x+1; i<=y; i++) {
-            if (i - n >= 0 && dp[i-n] != Integer.MAX_VALUE) dp[i] = Math.min(dp[i], dp[i-n]+1);
-            if (i % 2 == 0 && dp[i/2] != Integer.MAX_VALUE) dp[i] = Math.min(dp[i], dp[i/2]+1);            
-            if (i % 3 == 0 && dp[i/3] != Integer.MAX_VALUE) dp[i] = Math.min(dp[i], dp[i/3]+1);
+        for (int i=x; i<=y; i++) {
+            if (dp[i] == Integer.MAX_VALUE) continue;
+            
+            if (i+n <= y) dp[i+n] = Math.min(dp[i+n], dp[i]+1);
+            if (i*2 <= y) dp[i*2] = Math.min(dp[i*2], dp[i]+1);            
+            if (i*3 <= y) dp[i*3] = Math.min(dp[i*3], dp[i]+1);
         }
         
         return dp[y] == Integer.MAX_VALUE ? -1 : dp[y];
