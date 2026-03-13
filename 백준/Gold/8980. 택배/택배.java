06a734ec3ddd4ccfde6ad197f6913e93378116a5
Yeonsu <102608507+Yeonsu-Sim@users.noreply.github.com>
2026-03-13 22:58:25 +0900

    [Gold II] Title: 택배, Time: 108 ms, Memory: 14480 KB, Score: 52 point -BaekjoonHub

---
+++ 백준/Gold/8980. 택배/택배.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Box> pq = new PriorityQueue<>(
+            (Box a, Box b) -> a.length - b.length
+        );
+        
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cnt = Integer.parseInt(st.nextToken());
+            pq.offer(new Box(from, to, to-from, cnt));
+        }
+        
+        int[] truck = new int[N-1];
+        int sum = 0;
+        while (!pq.isEmpty()) {
+            Box cur = pq.poll();
+            int stuck = cur.cnt;
+            for (int i=cur.from-1; i<cur.to-1; i++) {
+                stuck = Math.min(stuck, C - truck[i]);
+            }
+
+            for (int i=cur.from-1; i<cur.to-1; i++) truck[i]+=stuck;
+            sum += stuck;
+        }
+
+        System.out.println(sum);
+    }
+
+    static class Box {
+        int from; int to; int length; int cnt;
+        Box(int from, int to, int length, int cnt) {
+            this.from = from;
+            this.to = to;
+            this.length = length;
+            this.cnt = cnt;
+        }
+    }
+}
