ad3395ab0c2936c49afbfe0e760fc51377dc3d4d
Yeonsu <102608507+Yeonsu-Sim@users.noreply.github.com>
2026-04-03 15:56:44 +0900

    [Gold III] Title: 탑 보기, Time: 560 ms, Memory: 39424 KB -BaekjoonHub

--- 백준/Gold/22866. 탑 보기/탑 보기.java
+++ 백준/Gold/22866. 탑 보기/탑 보기.java
@@ -1,54 +1,88 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+    static class Building {
+        int idx;
+        int height;
+
+        Building(int idx, int height) {
+            this.idx = idx;
+            this.height = height;
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N+1];
-        
+
+        int[] arr = new int[N + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i=1; i<=N; i++) {
+        for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        int[] leftCnt = new int[N + 1];
+        int[] rightCnt = new int[N + 1];
+        int[] leftNear = new int[N + 1];
+        int[] rightNear = new int[N + 1];
 
-        int[][] dp1 = new int[N+1][2];  // 오른쪽 보기(인덱스, 개수)
-        for (int i=N-1; i>0; i--) {
-            int j = i+1;
-            while (j<=N && j > 0 && arr[j] < arr[i]) j = dp1[j][0];
-            
-            if (j<=N && j > 0) {
-                dp1[i][0] = arr[j] == arr[i] ? dp1[j][0] : j;
-                dp1[i][1] = (arr[j] == arr[i] ? 0 : 1) + dp1[j][1];
+        Deque<Building> stack = new ArrayDeque<>();
+
+        // 왼쪽 보기
+        for (int i = 1; i <= N; i++) {
+            while (!stack.isEmpty() && stack.peek().height <= arr[i]) {
+                stack.pop();
             }
+
+            leftCnt[i] = stack.size();
+            if (!stack.isEmpty()) {
+                leftNear[i] = stack.peek().idx;
+            }
+
+            stack.push(new Building(i, arr[i]));
         }
 
+        stack.clear();
 
-        int[][] dp2 = new int[N+1][2];  // 왼쪽 보기
-        for (int i=2; i<=N; i++) {
-            int j = i-1;
-            while (j>0 && arr[j] < arr[i]) j = dp2[j][0];
-            
-            if (j>0) {
-                dp2[i][0] = arr[j] == arr[i] ? dp2[j][0] : j;
-                dp2[i][1] = (arr[j] == arr[i] ? 0 : 1) + dp2[j][1];
+        // 오른쪽 보기
+        for (int i = N; i >= 1; i--) {
+            while (!stack.isEmpty() && stack.peek().height <= arr[i]) {
+                stack.pop();
             }
+
+            rightCnt[i] = stack.size();
+            if (!stack.isEmpty()) {
+                rightNear[i] = stack.peek().idx;
+            }
+
+            stack.push(new Building(i, arr[i]));
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i=1; i<=N; i++) {
-            int cnt = dp1[i][1] + dp2[i][1];
-            sb.append(cnt+" ");
-            
-            int min = -1;
-            if (dp1[i][1] != 0) min = dp1[i][0];
-            if (dp2[i][1] != 0) {
-                if (min == -1 || min - i >= i - dp2[i][0]) min = dp2[i][0];
+
+        for (int i = 1; i <= N; i++) {
+            int total = leftCnt[i] + rightCnt[i];
+
+            if (total == 0) {
+                sb.append(0).append('\n');
+                continue;
             }
-            if (min != -1) sb.append(min);
-            sb.append("\n");
+
+            int ansIdx;
+            if (leftNear[i] == 0) {
+                ansIdx = rightNear[i];
+            } else if (rightNear[i] == 0) {
+                ansIdx = leftNear[i];
+            } else {
+                int leftDist = i - leftNear[i];
+                int rightDist = rightNear[i] - i;
+
+                if (leftDist <= rightDist) ansIdx = leftNear[i];
+                else ansIdx = rightNear[i];
+            }
+
+            sb.append(total).append(' ').append(ansIdx).append('\n');
         }
 
         System.out.print(sb);
