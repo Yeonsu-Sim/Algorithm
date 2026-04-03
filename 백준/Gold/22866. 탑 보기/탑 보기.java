f534ed8a572d675c2fd0bf1cc0839fc64aa76142
Yeonsu <102608507+Yeonsu-Sim@users.noreply.github.com>
2026-04-03 15:56:15 +0900

    [Gold III] Title: 탑 보기, Time: 484 ms, Memory: 39884 KB -BaekjoonHub

--- 백준/Gold/22866. 탑 보기/탑 보기.java
+++ 백준/Gold/22866. 탑 보기/탑 보기.java
@@ -14,40 +14,38 @@ public class Main {
         }
 
 
-        int[] dp1 = new int[N+1];  // 오른쪽 보기
-        int[] min1 = new int[N+1];
+        int[][] dp1 = new int[N+1][2];  // 오른쪽 보기(인덱스, 개수)
         for (int i=N-1; i>0; i--) {
             int j = i+1;
-            while (j!=0 && arr[j] <= arr[i]) j = min1[j];
+            while (j<=N && j > 0 && arr[j] < arr[i]) j = dp1[j][0];
             
-            if (j!=0) {
-                dp1[i] = 1 + dp1[j];
-                min1[i] = j;
+            if (j<=N && j > 0) {
+                dp1[i][0] = arr[j] == arr[i] ? dp1[j][0] : j;
+                dp1[i][1] = (arr[j] == arr[i] ? 0 : 1) + dp1[j][1];
             }
         }
 
 
-        int[] dp2 = new int[N+1];  // 왼쪽 보기
-        int[] min2 = new int[N+1];
+        int[][] dp2 = new int[N+1][2];  // 왼쪽 보기
         for (int i=2; i<=N; i++) {
             int j = i-1;
-            while (j!=0 && arr[j] <= arr[i]) j = min2[j];
+            while (j>0 && arr[j] < arr[i]) j = dp2[j][0];
             
-            if (j!=0) {
-                dp2[i] = 1 + dp2[j];
-                min2[i] = j;
+            if (j>0) {
+                dp2[i][0] = arr[j] == arr[i] ? dp2[j][0] : j;
+                dp2[i][1] = (arr[j] == arr[i] ? 0 : 1) + dp2[j][1];
             }
         }
 
         StringBuilder sb = new StringBuilder();
         for (int i=1; i<=N; i++) {
-            int cnt = dp1[i] + dp2[i];
+            int cnt = dp1[i][1] + dp2[i][1];
             sb.append(cnt+" ");
             
             int min = -1;
-            if (dp1[i] != 0) min = min1[i];
-            if (dp2[i] != 0) {
-                if (min == -1 || min - i >= i - min2[i]) min = min2[i];
+            if (dp1[i][1] != 0) min = dp1[i][0];
+            if (dp2[i][1] != 0) {
+                if (min == -1 || min - i >= i - dp2[i][0]) min = dp2[i][0];
             }
             if (min != -1) sb.append(min);
             sb.append("\n");
